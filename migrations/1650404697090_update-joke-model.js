01eb633a074563c115d77a1648f961de234ce84a
1uy31 <snow.tuyet.tran.31@gmail.com>
2022-04-20 00:49:36 +0300

    Update joke model

---
+++ migrations/1650404697090_update-joke-model.js
@@ -0,0 +1,16 @@
+/* eslint-disable camelcase */
+
+const { PgLiteral } = require('node-pg-migrate');
+exports.shorthands = undefined;
+
+exports.up = async (pgm) => {
+  await pgm.sql(`
+    ALTER TABLE joke
+    ADD COLUMN user_id uuid
+    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES user(id)`);
+};
+
+/**
+ * Ignore on purpose.
+ */
+exports.down = (pgm) => {};
