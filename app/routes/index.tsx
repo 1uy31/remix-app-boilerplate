43195948d204974c5598af8a58b23e3e875c8211
1uy31 <snow.tuyet.tran.31@gmail.com>
2022-04-25 23:31:47 +0300

    Finalise index page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,28 @@
 import stylesUrl from '~/styles/index.css';
 import { LinksFunction } from '@remix-run/node';
+import { Link } from '@remix-run/react';
 
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: stylesUrl }];
 };
 
-const IndexRoute = () => {
-  return <div>Hello Index Route</div>;
+const Index = () => {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          <span>Hello!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
-export default IndexRoute;
+export default Index;
